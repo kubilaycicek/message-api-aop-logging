d5ed9afc5fc7a0eecf29cafb1f7b3225f4558be0
kubilaycicek <kubilaycicek0693@gmail.com>
2021-09-02 12:26:35 +0300

    Enable Slf4j Log

--- spring-boot-aop-test/src/main/java/com/kubilaycicek/springbootaoptest/aop/ServiceAspect.java
+++ spring-boot-aop-test/src/main/java/com/kubilaycicek/springbootaoptest/aop/ServiceAspect.java
@@ -1,38 +1,37 @@
 package com.kubilaycicek.springbootaoptest.aop;
 
+import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.*;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
+@Slf4j
 public class ServiceAspect {
 
-	@Before("execution(* com.kubilaycicek.springbootaoptest.service.*.*(..))")
-	public void before(JoinPoint joinPoint) {
-		String log = new StringBuilder().append("Before").append("Signature : " + joinPoint.getSignature() + " ").append("This :" + joinPoint.getThis()).toString();
+    private static final String SIGNATURE_TEXT = "Signature : ";
+    private static final String THIS_TEXT = "This : ";
 
-		System.out.println(log);
-	}
+    @Before("execution(* com.kubilaycicek.springbootaoptest.service.*.*(..))")
+    public void before(JoinPoint joinPoint) {
 
-	@After("execution(* com.kubilaycicek.springbootaoptest.service.*.*(..))")
-	public void after(JoinPoint joinPoint) {
-		String log = new StringBuilder().append("After").append("Signature : " + joinPoint.getSignature() + " ").append("This :" + joinPoint.getThis()).toString();
+        log.info(new StringBuilder().append("Before").append(SIGNATURE_TEXT + joinPoint.getSignature() + " ").append(THIS_TEXT + joinPoint.getThis()).toString());
+    }
 
-		System.out.println(log);
-	}
+    @After("execution(* com.kubilaycicek.springbootaoptest.service.*.*(..))")
+    public void after(JoinPoint joinPoint) {
+        log.info(new StringBuilder().append("After").append(SIGNATURE_TEXT + joinPoint.getSignature() + " ").append(THIS_TEXT + joinPoint.getThis()).toString());
+    }
 
-	@AfterReturning("execution(* com.kubilaycicek.springbootaoptest.service.*.*(..))")
-	public void afterReturning(JoinPoint joinPoint) {
-		String log = new StringBuilder().append("After Returning").append("Signature : " + joinPoint.getSignature() + " ").append("This :" + joinPoint.getThis()).toString();
+    @AfterReturning("execution(* com.kubilaycicek.springbootaoptest.service.*.*(..))")
+    public void afterReturning(JoinPoint joinPoint) {
+        log.info(new StringBuilder().append("After Returning").append(SIGNATURE_TEXT + joinPoint.getSignature() + " ").append(THIS_TEXT + joinPoint.getThis()).toString());
+    }
 
-		System.out.println(log);
-	}
-
-	@AfterThrowing("execution(* com.kubilaycicek.springbootaoptest.service.*.*(..))")
-	public void afterThrowing(JoinPoint joinPoint) {
-		String log = new StringBuilder().append("After Throwing").append("Signature : " + joinPoint.getSignature() + " ").append("This :" + joinPoint.getThis()).toString();
-		System.out.println(log);
-	}
+    @AfterThrowing("execution(* com.kubilaycicek.springbootaoptest.service.*.*(..))")
+    public void afterThrowing(JoinPoint joinPoint) {
+        log.error(new StringBuilder().append("After Throwing").append(SIGNATURE_TEXT + joinPoint.getSignature() + " ").append(THIS_TEXT + joinPoint.getThis()).toString());
+    }
 
 }
